634f1aa4a752951ea355622f2427c9e4d7c3b30f
antonio-pas <antonio.b.passos18@gmail.com>
2024-06-12 19:54:34 -0700

    Starting

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen">
+      <ul className="menu">
+        <img
+          src="https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=300"
+          className="w-12 h-12 aspect-square rounded-full object-cover" 
+        />
+        <li>
+        </li>
+      </ul>
+    </div>
+  )
+}
